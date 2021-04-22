e87f52face491fbf8957342e58d33f8f8eb59c32
qiushibo <1114676346@qq.comcom>
2021-04-22 20:37:38 +0800

    历史记录界面和文本输入界面

--- app/src/main/java/com/example/infoclassfy/MainActivity.java
+++ app/src/main/java/com/example/infoclassfy/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.infoclassfy;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_fileInit;
+    public static final String XLS = "application/vnd.ms-excel application/x-excel";
+    public static final String XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_fileInit=findViewById(R.id.fileInit);
+
+
         btn_fileInit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 //                 //intent.setDataAndType(Uri.fromFile(dir.getParentFile()), "file/*.txt");
                 //intent.setType("file/*.txt"); //华为手机mate7不支持
-                intent.setType("text/plain");
+                intent.setType("text/plain|XLS|XLSX");
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-               intent.setType("file/*.txt;file/*xls");
+
                 startActivityForResult(intent, 0);
 
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(getApplicationContext(),"文件的内容是："+String.valueOf(resultCode),Toast.LENGTH_LONG);
+        Log.d("2222", String.valueOf(data));
     }
 }
---
+++ app/src/main/java/com/example/infoclassfy/activity/HistoryInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.infoclassfy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.infoclassfy.R;
+
+public class HistoryInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_info);
+    }
+}
